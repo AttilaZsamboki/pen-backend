2d2c6e29ceb48edfaf929730ad1b6417a5b2cfc9
atti-at-274951945201 <zsamboki.attila.jr@gmail.com>
2023-08-14 13:32:40 +0200

    minicrm webhook url átnevezés

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pen_minicrm_webhook/', views.PenCalculateDistance.as_view()),
-    path('pen_googlesheet_webhook/', views.PenGoogleSheetWebhook.as_view()),
+    path('minicrm_webhook/', views.PenCalculateDistance.as_view()),
+    path('googlesheet_webhook/', views.PenGoogleSheetWebhook.as_view()),
     path('cron/szamlazz/', views.CronSzamlazz.as_view()),
 ]
