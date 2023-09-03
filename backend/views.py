64957257d16a09f81037f6bfcb7bd416d9251634
atti-at-274951945201 <zsamboki.attila.jr@gmail.com>
2023-09-03 21:23:56 +0200

    removed global pagination

--- backend/views.py
+++ backend/views.py
@@ -11,6 +11,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_201_CREATED
 from rest_framework.permissions import AllowAny
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import generics
 
 import json
@@ -118,10 +119,12 @@ class OrderWebhook(APIView):
         log("Penészmentesítés rendelés webhook meghívva", "INFO", "pen_order_webhook", request.body)
         return Response("Succesfully received data", status=HTTP_200_OK)
 
+
 class ProductsList(generics.ListCreateAPIView):
     queryset = models.Products.objects.all()
     serializer_class = serializers.ProductsSerializer
     permission_classes = [AllowAny]
+    pagination_class = LimitOffsetPagination
 
 class ProductsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Products.objects.all()
