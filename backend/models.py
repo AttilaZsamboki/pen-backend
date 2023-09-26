3daaea84fdb8f092d7125ad9b6db43889aad4acd
atti-at-274951945201 <zsamboki.attila.jr@gmail.com>
2023-09-26 13:34:52 +0200

    products structure change

--- backend/models.py
+++ backend/models.py
@@ -35,7 +35,7 @@ class FelmeresNotes(models.Model):
         db_table = "pen_felmeresek_notes"
 
 class Products(models.Model):
-    id = models.BigIntegerField(db_column='ID', blank=True, primary_key=True)  # Field name made lowercase.
+    id = models.BigIntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
     name = models.TextField(db_column='Name', blank=True, null=True)  # Field name made lowercase.
     sku = models.TextField(db_column='SKU', blank=True, null=True)  # Field name made lowercase.
     type = models.TextField(db_column='Type', blank=True, null=True)  # Field name made lowercase.
@@ -46,9 +46,9 @@ class Products(models.Model):
     category = models.TextField(db_column='Category', blank=True, null=True)  # Field name made lowercase.
     parent_id = models.FloatField(db_column='Parent_ID', blank=True, null=True)  # Field name made lowercase.
     parent_sku = models.FloatField(db_column='Parent_SKU', blank=True, null=True)  # Field name made lowercase.
-    bundles_sku = models.FloatField(db_column='Bundles_SKU', blank=True, null=True)  # Field name made lowercase.
-    description = models.FloatField(db_column='Description', blank=True, null=True)  # Field name made lowercase.
-    short_description = models.FloatField(db_column='Short_Description', blank=True, null=True)  # Field name made lowercase.
+    bundles_sku = models.FloatField(db_column='Bundles_SKU', blank=True, null=True)  # Field name made lowercase.       
+    description = models.TextField(db_column='Description', blank=True, null=True)  # Field name made lowercase.        
+    short_description = models.TextField(db_column='Short_Description', blank=True, null=True)  # Field name made lowercase.
     images = models.BigIntegerField(db_column='Images', blank=True, null=True)  # Field name made lowercase.
     unit = models.FloatField(db_column='Unit', blank=True, null=True)  # Field name made lowercase.
     moq = models.FloatField(db_column='MOQ', blank=True, null=True)  # Field name made lowercase.
@@ -59,7 +59,7 @@ class Products(models.Model):
     weight = models.BigIntegerField(db_column='Weight', blank=True, null=True)  # Field name made lowercase.
     net_weight = models.FloatField(db_column='Net_Weight', blank=True, null=True)  # Field name made lowercase.
     webshop_sort_order = models.BigIntegerField(db_column='Webshop_Sort_Order', blank=True, null=True)  # Field name made lowercase.
-    commodity_code = models.FloatField(db_column='Commodity_Code', blank=True, null=True)  # Field name made lowercase.
+    commodity_code = models.FloatField(db_column='Commodity_Code', blank=True, null=True)  # Field name made lowercase. 
     excisable_product = models.BigIntegerField(db_column='Excisable_Product', blank=True, null=True)  # Field name made lowercase.
     combined_nomenclature = models.FloatField(db_column='Combined_Nomenclature', blank=True, null=True)  # Field name made lowercase.
     quantity_multiplier = models.FloatField(db_column='Quantity_Multiplier', blank=True, null=True)  # Field name made lowercase.
@@ -68,16 +68,16 @@ class Products(models.Model):
     warranty_period = models.FloatField(db_column='Warranty_Period', blank=True, null=True)  # Field name made lowercase.
     warranty_period_unit = models.TextField(db_column='Warranty_Period_Unit', blank=True, null=True)  # Field name made lowercase.
     virtual = models.BigIntegerField(db_column='Virtual', blank=True, null=True)  # Field name made lowercase.
-    fragile = models.BigIntegerField(db_column='Fragile', blank=True, null=True)  # Field name made lowercase.
-    product_class = models.FloatField(db_column='Product_Class', blank=True, null=True)  # Field name made lowercase.
-    upsell_products = models.FloatField(db_column='Upsell_Products', blank=True, null=True)  # Field name made lowercase.
-    upsell_categories = models.FloatField(db_column='Upsell_Categories', blank=True, null=True)  # Field name made lowercase.
+    fragile = models.TextField(db_column='Fragile', blank=True, null=True)  # Field name made lowercase.
+    product_class = models.TextField(db_column='Product_Class', blank=True, null=True)  # Field name made lowercase.    
+    upsell_products = models.TextField(db_column='Upsell_Products', blank=True, null=True)  # Field name made lowercase.
+    upsell_categories = models.TextField(db_column='Upsell_Categories', blank=True, null=True)  # Field name made lowercase.
     tags = models.FloatField(db_column='Tags', blank=True, null=True)  # Field name made lowercase.
     virtual_net_cost = models.FloatField(db_column='Virtual_Net_Cost', blank=True, null=True)  # Field name made lowercase.
     virtual_net_cost_currency = models.TextField(db_column='Virtual_Net_Cost_Currency', blank=True, null=True)  # Field name made lowercase.
-    tracking_type = models.TextField(db_column='Tracking_Type', blank=True, null=True)  # Field name made lowercase.
-    manufacturer = models.FloatField(db_column='Manufacturer', blank=True, null=True)  # Field name made lowercase.
-    manufacturer_sku = models.FloatField(db_column='Manufacturer_Sku', blank=True, null=True)  # Field name made lowercase.
+    tracking_type = models.TextField(db_column='Tracking_Type', blank=True, null=True)  # Field name made lowercase.    
+    manufacturer = models.TextField(db_column='Manufacturer', blank=True, null=True)  # Field name made lowercase.      
+    manufacturer_sku = models.TextField(db_column='Manufacturer_Sku', blank=True, null=True)  # Field name made lowercase.
     price_list_alapertelmezett_net_price_huf = models.BigIntegerField(db_column='Price_List___alapertelmezett___Net_Price_HUF', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     price_list_alapertelmezett_price_huf = models.FloatField(db_column='Price_List___alapertelmezett___Price_HUF', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     price_list_alapertelmezett_vat = models.TextField(db_column='Price_List___alapertelmezett___VAT', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
@@ -90,15 +90,15 @@ class Products(models.Model):
     sale_price_list_alapertelmezett_vat = models.FloatField(db_column='Sale_Price_List___alapertelmezett___VAT', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     sale_price_list_alapertelmezett_vat_field = models.FloatField(db_column='Sale_Price_List___alapertelmezett___VAT_', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row. Field renamed because it ended with '_'.
     sale_price_list_alapertelmezett_from_date = models.FloatField(db_column='Sale_Price_List___alapertelmezett___From_Date', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
-    sale_price_list_alapertelmezett_to_date = models.FloatField(db_column='Sale_Price_List___alapertelmezett___To_Date', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
+    sale_price_list_alapertelmezett_to_date = models.FloatField(db_column='Sale_Price_List___alapertelmezett___To_Date', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.    
     sale_price_list_alapertelmezett_quantity_discount = models.FloatField(db_column='Sale_Price_List___alapertelmezett___Quantity_Discount', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
-    sale_price_list_alapertelmezett_formula = models.FloatField(db_column='Sale_Price_List___alapertelmezett___Formula', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
+    sale_price_list_alapertelmezett_formula = models.FloatField(db_column='Sale_Price_List___alapertelmezett___Formula', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.    
     sale_price_list_alapertelmezett_price_type = models.FloatField(db_column='Sale_Price_List___alapertelmezett___Price_Type', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     warehouse_raktar_1_allowed = models.BigIntegerField(db_column='Warehouse___Raktar_1___Allowed', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     warehouse_raktar_1_minimum_stock_quantity = models.FloatField(db_column='Warehouse___Raktar_1____Minimum_Stock_Quantity', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     warehouse_raktar_1_optimal_stock_quantity = models.FloatField(db_column='Warehouse___Raktar_1____Optimal_Stock_Quantity', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     warehouse_selejt_allowed = models.BigIntegerField(db_column='Warehouse___Selejt___Allowed', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
-    warehouse_selejt_minimum_stock_quantity = models.FloatField(db_column='Warehouse___Selejt____Minimum_Stock_Quantity', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
+    warehouse_selejt_minimum_stock_quantity = models.FloatField(db_column='Warehouse___Selejt____Minimum_Stock_Quantity', blank=True, null=True)  # Field, blank=True, null=True)  # Fi, blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
     warehouse_selejt_optimal_stock_quantity = models.FloatField(db_column='Warehouse___Selejt____Optimal_Stock_Quantity', blank=True, null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
 
     class Meta:
