20f49383c3679158897a0a057bb320203a1d37b5
atti-at-274951945201 <zsamboki.attila.jr@gmail.com>
2023-10-04 18:44:19 +0200

    ERP ORDERID és URL visszaírása a megrendelésre

---
+++ backend/cron/erp_order_id.py
@@ -0,0 +1,26 @@
+from ..utils.minicrm import get_all_adatlap_details, update_offer_order
+from ..utils.logs import log
+from ..models import Orders, Order
+
+def main():
+    log("MiniCRM ERP rendelésszám szinkron megkezdődött", "INFO", "pen_erp_order_id")
+    adatlapok = get_all_adatlap_details(29, criteria=lambda adatlap: not adatlap["ClouderpMegrendeles"] or not adatlap["RendelesSzama"])
+    for adatlap in adatlapok:
+
+        order_id = Orders.objects.filter(adatlap_id=adatlap["Id"]).first()
+        if not order_id:
+            log("Nem található megfelelő rendelés a megrendeléshez", "WARNING", "pen_erp_order_id")
+            continue
+        order_id = order_id.order_id
+
+        order = Order.objects.filter(webshop_id=order_id).first()
+        if not order:
+            log("Nem található megfelelő rendelés a megrendeléshez", "WARNING", "pen_erp_order_id")
+            continue
+        resp = update_offer_order(order.webshop_id, {"ClouderpMegrendeles": "https://app.clouderp.hu/v2/order?search="+str(order.order_id), "RendelesSzama": order.order_id}, type="Order")
+        if resp.ok:
+            log("Rendelésszám sikeresen frissítve", "SUCCESS", "pen_erp_order_id", order_id)
+        else:
+            log("Rendelésszám frissítése sikertelen", "ERROR", "pen_erp_order_id", f"OrderId: {order_id}. Response: {resp.text}")
+
+main()
