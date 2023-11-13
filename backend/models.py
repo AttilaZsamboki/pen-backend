01112d7ed9225eb1ee481cb0d0559f031a00175e
atti-at-274951945201 <zsamboki.attila.jr@gmail.com>
2023-11-13 13:30:32 +0100

    ammount

--- backend/models.py
+++ backend/models.py
@@ -1707,6 +1707,7 @@ class Munkadij(models.Model):
 class FelmeresMunkadijak(models.Model):
     felmeres = models.ForeignKey("Felmeresek", models.DO_NOTHING, blank=True, null=True)
     munkadij = models.ForeignKey("Munkadij", models.DO_NOTHING, blank=True, null=True)
+    ammount = models.IntegerField()
 
     class Meta:
         managed = False
